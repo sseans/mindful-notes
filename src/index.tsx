2fcedfed4ecb983ed0a9f3892eadb837fd5a7849
sseans <41116820+sseans@users.noreply.github.com>
2021-08-01 11:39:27 +1000

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import GlobalStyle from "./Styles/GlobalStyle.js";
+import GlobalStyle from "./Styles/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
