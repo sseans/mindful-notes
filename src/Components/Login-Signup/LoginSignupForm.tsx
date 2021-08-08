26f819a41212b421e93c8612df5a16fe649cfd3e
sseans <41116820+sseans@users.noreply.github.com>
2021-08-08 16:03:25 +1000

    unsaved changes

--- src/Components/Login-Signup/LoginSignupForm.tsx
+++ src/Components/Login-Signup/LoginSignupForm.tsx
@@ -47,8 +47,6 @@ export const LoginSignupForm = ({
 }: loginPropsType) => {
   const [formDetails, setFormDetails] = useState({});
 
-  function formSubmit() {}
-
   return (
     <SignupContainer
       onSubmit={(e) => {
